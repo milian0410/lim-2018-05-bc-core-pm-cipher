eb7037b8198fd1f7a96988ca70afd59c5dd0cf4f
milian0410 <milian0410@hotmail.com>
2018-05-29 18:39:07 -0500

    resultado de encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,23 @@
 window.cipher = {
-encode : function(cadena) {
-    let cadenaNew;
-    const valor = 33;
-    for(let i=0; i<cadena.length; i++) {
-        let caracter= cadena.charCodeAt(i);
-        let caracterNew=((caracter-65+valor)%26)+65;
-    cadenaNew += String.fromCharCode(caracterNew);
-    }
+	encode: (offset, cadena)=> {
+		let cadenaNew="";
+		for(let i=0; i<cadena.length; i++) {
+			let caracter= cadena.charCodeAt(i);
+			let caracterNew=(caracter-65+offset)%26+65;
+			cadenaNew += String.fromCharCode(caracterNew);
+		}
+		return cadenaNew;
+	}
 }
+	/*
+	decode: (offset,cadena)
+	let cadenaNew="";
+		for(let i=0; i<cadena.length; i++) {
+			let caracter= cadena.charCodeAt(i);
+			let caracterNew=(caracter+65+offset)%26+65;
+			cadenaNew += String.fromCharCode(caracterNew);
+		}
+		return cadenaNew;
+	}
 }
+*/
--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
 
-let cadena = document.getElementById("message");
-let Offset = document.getElementById("Offset");
-let botonEncode = document.getElementById("Encriptar");
-botonEncode.addEventListener("click", llamarAlCodigo());
+//variable del texto//
+let text = document.getElementById("message");
+//variable del desplazamiento//
+let offset = document.getElementById("offset");
+//variable del boton cifrar//
+const botonEncode = document.getElementById("encode");
+//variable del boton para descifrar//
+const botonDecode = document.getElementById("decode");
+//variable del resultado//
+const result = document.getElementById("answer");
+//variable del boton limpiar//
+const botonCleaner = document.getElementById("clean");
 
-function llamarAlCodigo()
-{
-alert("estoy feliz");
+//llamar a la función con el botón cifrar//
+botonEncode.addEventListener ("click", ()=> {
+    result.innerHTML = cipher.encode(parseInt(offset.value), text.value);
+});
+console.log(result.value);
+
+/*
+//llamar a la función con el botón descifrar//
+botonDecode.addEventListener ("click", ()=> {
+    result.value = cipher.decode(parseInt(offset.value), text.value);
+});
+console.log(result.value);*/
+
+//llamar a la función con el boton limpiar//
+function myFunction() {
+    document.getElementById("message").innerHTML = "";
 }
 
+//botonEncode.addEventListener ("click", clickBotonEncode);//
+/*
+function clickBotonEncode() {
+    let cadenaNew="";
+    for(let i=0; i<cadena.length; i++) {
+        let caracter= cadena.charCodeAt(i);
+        let caracterNew=(caracter-65+offset)%26+65;
+    cadenaNew += String.fromCharCode(caracterNew);
+    }
+document.getElementById("answer").innerHTML = cadenaNew.value;
+}
+*/
+/*
+let cadenaNew ='';
 
-
+      for(let i=0; i<cadena.length; i++) {
+        let caracter= cadena.charCodeAt(i);
+        let caracterNew=(caracter-65+1)%26+65;
+         cadenaNew = cadenaNew + String.fromCharCode(caracterNew);*/
