ce61261a1725f760d2bedf9af551d60b6e0dd51f
milian0410 <milian0410@hotmail.com>
2018-05-29 08:21:58 -0500

    tercera modificacion index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,11 @@
-window.cipher = {
-encode : function(cadena) {
-    let cadenaNew;
-    const valor = 33;
-    for(let i=0; i<cadena.length; i++) {
+//window.cipher = {
+//encode : 
+/*let cadena="ABC";
+let cadenaNew ='';
+
+      for(let i=0; i<cadena.length; i++) {
         let caracter= cadena.charCodeAt(i);
-        let caracterNew=((caracter-65+valor)%26)+65;
-    cadenaNew += String.fromCharCode(caracterNew);
-    }
-}
-}
+        let caracterNew=(caracter-65+1)%26+65;
+         cadenaNew = cadenaNew + String.fromCharCode(caracterNew);*/
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 
-let cadena = document.getElementById(message);
-let result = document.getElementById("encriptar").addEventListener("click", window.cipher.encode(cadena) )
+let cadena = document.getElementById("message").value;
+let offSet = document.getElementById("offset").value;
+const botonEncode = document.getElementById("encode");
+botonEncode.addEventListener ("click", clickBotonEncode);
+let result = document.getElementById("answer");
+
+
+function clickBotonEncode() {
+    let cadenaNew="";
+    for(let i=0; i<cadena.length; i++) {
+        let caracter= cadena.charCodeAt(i);
+        let caracterNew=(caracter-65+offset)%26+65;
+    cadenaNew += String.fromCharCode(caracterNew);
+    }
+document.getElementById("answer").innerHTML = cadenaNew.value;
+}
+
+/*
+let cadenaNew ='';
+
+      for(let i=0; i<cadena.length; i++) {
+        let caracter= cadena.charCodeAt(i);
+        let caracterNew=(caracter-65+1)%26+65;
+         cadenaNew = cadenaNew + String.fromCharCode(caracterNew);*/
